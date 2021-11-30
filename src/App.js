9ccd300d782f8c2dacc48afc05493bfeacf658a0
Adam Burgess <aburg1515@gmail.com>
2021-11-30 09:15:05 -0700

    Add onclick event to form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Ideas from './Ideas';
+import Form from './Form'
 import './App.css';
 
 class App extends Component {
@@ -7,17 +8,22 @@ class App extends Component {
     super()
     this.state = {
       ideas: [
-        { id: 1, title: 'Prank Travis', description: 'Stick googly eyes on all his stuff' },
-        { id: 2, title: 'Make a secret password app', description: 'So you and your rideshare driver can both know neither one of you is lying' },
-        { id: 3, title: 'Learn a martial art', description: 'To exact vengeance upon my enemies' },
+        // { id: 1, title: 'Prank Travis', description: 'Stick googly eyes on all his stuff' },
+        // { id: 2, title: 'Make a secret password app', description: 'So you and your rideshare driver can both know neither one of you is lying' },
+        // { id: 3, title: 'Learn a martial art', description: 'To exact vengeance upon my enemies' },
       ]
     }
+  }
+
+  addIdea = (newIdea) => {
+    this.setState({ ideas: [...this.state.ideas, newIdea]})
   }
 
   render() {
     return (
       <main className='App'>
         <h1>IdeaBox</h1>
+        <Form addIdea={this.addIdea}/>
         {!this.state.ideas.length && <h2>No ideas yet -- add some!</h2>}
         <Ideas ideas={this.state.ideas}/>
       </main>
